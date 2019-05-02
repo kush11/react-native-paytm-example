b1ca0b2fad8a45619d3cf750ee6f0f598ce3c56d
Kush Kumar <kush.kumar@pratian.com>
2019-05-02 12:58:09 +0530

    app is working with the node server

--- App.js
+++ App.js
@@ -3,48 +3,45 @@ import { View, Text, TouchableOpacity, Modal, WebView } from "react-native";
 
 export default class App extends React.Component {
   state = {
-    showModa: false,
-    ORDER_ID: "KUSH66634",
+    showModal: false,
+    ORDER_ID: "ORDER_ID124344",
     TXN_AMOUNT: "500",
     CUST_ID: "Kush",
-    ack:''
+    ack: ''
   };
 
   handelResponse = (data) => {
     console.log(data);
-    if(data === 'true')
-    {
-      this.setState({showModa:false, ack:'Transaction was sucessfull'})
+    if (data === 'true') {
+      this.setState({ showModal: false, ack: 'Transaction was sucessfull' })
     }
-    else if(data === 'false'){
-      this.setState({showModa:false, ack:'Oop\'s Something went wrong'});
+    else if (data === 'false') {
+      this.setState({ showModal: false, ack: 'Oop\'s Something went wrong' });
     }
     else return;
   };
   render() {
-    const { ORDER_ID, TXN_AMOUNT, CUST_ID, showModa, ack } = this.state;
+    const { ORDER_ID, TXN_AMOUNT, CUST_ID, showModal, ack } = this.state;
     return (
       <View style={{ marginTop: 20 }}>
-        <TouchableOpacity onPress={() => this.setState({ showModa: true })}>
+        <TouchableOpacity onPress={() => this.setState({ showModal: true })}>
           <Text>Pay with Paytm</Text>
         </TouchableOpacity>
-        <View style={{marginTop:10}}>
+        <View style={{ marginTop: 10 }}>
           <Text>
             {ack}
           </Text>
         </View>
         <Modal
-          visible={showModa}
-          onRequestClose={() => this.setState({ showModa: true })}
+          visible={showModal}
+          onRequestClose={() => this.setState({ showModal: true })}
         >
           <WebView
-            source={{uri: '192.168.31.1:3001/api/paytm/request'}}            
-            injectedJavaScript={`document.getElementById('ORDER_ID).value="${ORDER_ID}";
-                document.getElementById('CUST_ID).value="${CUST_ID}";
-                document.getElementById('TXN_AMOUNT).value="${TXN_AMOUNT}";
-                document.f1.submit()`}
+            style={{ marginTop: 20 }}
+            source={{ uri: 'http://172.30.15.211:3002/api/paytm/request' }}
+            injectedJavaScript={`document.getElementById("ORDER_ID").value = "${ORDER_ID}";document.getElementById("CUST_ID").value = "${CUST_ID}";document.getElementById("TXN_AMOUNT").value = "${TXN_AMOUNT}";document.f1.submit();`}
             onNavigationStateChange={data => this.handelResponse(data.title)}
-          />          
+          />
         </Modal>
       </View>
     );
